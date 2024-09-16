30640d292c75993fc012ea44ff14feffb2a62834
cheny56 <cheny56@naver.com>
2024-09-16 18:00:13 +0900

    Update charger.js

--- CHARGER/charger.js
+++ CHARGER/charger.js
@@ -7,11 +7,20 @@ CHARGE , NOP
 */
 
 const db= require ( '../models')
+const { conv_array_to_object }= require( '../utils/common')
+const { findall } = require ('../utils/db')
+let CHARGE_PERIOD_IN_SEC = 60
+let CHARGE_UPTO_TARGET_AMOUNT = 100_0000_0000
 const main = async ()=>{
-  let 
+  let resp = await findall ( 'settings' , {} )
+  let jsettings = conv_array_to_object ( {arr: resp , keyfieldname : 'key' , valuefieldname:'value'})
+  if (jsettings[ 'CHARGE_PERIOD_IN_SEC' ] && +jsettings[ 'CHARGE_PERIOD_IN_SEC' ] ){ CHARGE_PERIOD_IN_SEC = +jsettings['CHARGE_PERIOD_IN_SEC'] }
+  else {}
+  if (jsettings[ 'CHARGE_UPTO_TARGET_AMOUNT' ] && +jsettings[ 'CHARGE_UPTO_TARGET_AMOUNT' ] ){ CHARGE_UPTO_TARGET_AMOUNT = +jsettings['CHARGE_UPTO_TARGET_AMOUNT'] }
+  else {}
   let h = setInterval ( async ()=>{
-
-  } , )
+    
+  } ,  CHARGE_PERIOD_IN_SEC )
 }
 
 main ()
