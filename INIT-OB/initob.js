2a1504c3f506dfd09786660d1168eb5e60645188
continent2 <appnweb8@gmail.com>
2024-09-17 18:24:06 +0900

    init orderbook works to some extent-20240917

--- INIT-OB/initob.js
+++ INIT-OB/initob.js
@@ -2,43 +2,66 @@
 const db = require( '../models' )
 const axios = require( 'axios' )
 const { getRandomInt  , get_random_float } = require ( '../utils/math' )
-let list_tp = [ 'BTC_USDT' , 'ETH_USDT' ]
+const { parse_orderbook } = require ( '../utils/exchanges/binance' )
+let list_tp = [ 'BTC_USDT' ]
+// let list_tp = [ 'BTC_USDT' , 'ETH_USDT' ]
 const BIN_EP_SPOT_TICKER = `https://api.binance.com/api/v3/ticker/price?symbol=`
 // EX: `https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT`
 // resp.data => { symbol: 'BTCUSDT', price: '57727.99000000' }
 
-const BIN_EP_ORDERBOOK = `https://api.binance.com/api/v3/depth?limit=10&symbol=`
+const BIN_EP_ORDERBOOK = `https://api.binance.com/api/v3/depth` // ?limit=10&symbol=`
 // https://api.binance.com/api/v3/depth?limit=10&symbol=BTCUSDT
 
-let N_ORDER_BINS_A_SIDE = 100
+let N_BINANCE_ORDERBOOK_QUERY_COUNT = 40
+// let N_ORDER_BINS_A_SIDE = 10
+let N_ORDER_BINS_A_SIDE = 40
+// let N_ORDER_BINS_A_SIDE = 100
 let N_MAX_ORDERS_A_BIN = 2
-const place_order_sell =async ( { } )=>{
+let MAX_ORDER_AMOUNT = 10 
+let MIN_ORDER_AMOUNT = 0.1
+let DIVIDER_4_STEPSIZE = 10000
+const place_order = ( { type , price , amount } )=>{ // type : 'buy'  , 'sell'
+  console.log ( 'ORDER' , type , price , amount )
 }
 
-const main = async ( { } )=>{
-
+const main = async ( )=>{
   for ( let idxsymbols = 0 ; idxsymbols < list_tp?.length ; idxsymbols ++ ){
     let tickersymbol = list_tp [ idxsymbols ]
     tickersymbol = tickersymbol.replace ( /_/g, '' )
-    let ep = `${ BIN_EP_SPOT_TICKER }${ tickersymbol }`
+    let ep = `${ BIN_EP_ORDERBOOK }` //    let ep = `${ BIN_EP_SPOT_TICKER }${ tickersymbol }`
     try {
-      let resp = await axios.get ( ep )
-      if ( resp?.data?.price ){
-        let { price : midprice } = resp?.data        
-        let stepsize = +price / N_ORDER_BINS_A_SIDE
+      let resp = await axios.get ( ep , { params : { limit : N_BINANCE_ORDERBOOK_QUERY_COUNT , symbol : tickersymbol }} )
+      if ( resp?.data?.asks?.length ){ //        let { price : midprice } = resp?.data
+        let { midprice , buy_volume , sell_volume } = parse_orderbook ( { j_ob : resp?.data })
+        console.log ( { midprice , buy_volume , sell_volume } )         
+        let stepsize = +midprice / DIVIDER_4_STEPSIZE
+        // let stepsize = +midprice / N_ORDER_BINS_A_SIDE
+        /** SELL */
         for ( let idxbin = 0 ; idxbin < N_ORDER_BINS_A_SIDE ; idxbin ++ ) {
           let border_low = midprice + ( 1 + idxbin ) * stepsize
           let border_high= midprice + ( 2 + idxbin ) * stepsize
           let n_orders = getRandomInt ( 1 , N_MAX_ORDERS_A_BIN )
           for ( let idxorder = 0 ; idxorder < n_orders ; idxorder ++ ){
             let orderprice = get_random_float ( { max : border_high , min: border_low })
+            let orderamount= get_random_float ( { max : MAX_ORDER_AMOUNT , min : MIN_ORDER_AMOUNT })
+             place_order ( { type: 'sell' , price : orderprice , amount : orderamount })
+          }
+        }
+        /** BUY  */
+        for ( let idxbin = 0 ; idxbin < N_ORDER_BINS_A_SIDE ; idxbin ++ ) {
+          let border_low = midprice - ( 2 + idxbin ) * stepsize
+          let border_high= midprice - ( 1 + idxbin ) * stepsize
+          let n_orders = getRandomInt ( 1 , N_MAX_ORDERS_A_BIN )
+          for ( let idxorder = 0 ; idxorder < n_orders ; idxorder ++ ){
+            let orderprice = get_random_float ( { max : border_high , min: border_low })
+            let orderamount= get_random_float ( { max : MAX_ORDER_AMOUNT , min : MIN_ORDER_AMOUNT })
+            await place_order ( { type: 'buy' , price : orderprice , amount : orderamount })
           }
         }
       }
       else { }
     }
-    catch {}    
-
+    catch {}
   }
 }
 
---
+++ utils/exchanges/binance.js
@@ -0,0 +1,124 @@
+
+// const BIN_EP_ORDERBOOK = `https://api.binance.com/api/v3/depth?limit=10&symbol=`
+// https://api.binance.com/api/v3/depth?limit=10&symbol=BTCUSDT
+
+const parse_orderbook = ({ j_ob })=>{
+  let buy_prices = j_ob?.bids.map ( elem => +elem[ 0 ])
+  buy_prices.sort() // ascending
+  let buy_price_head = buy_prices[ buy_prices?.length -1 ]
+
+  let sell_prices = j_ob?.asks.map ( elem => +elem[ 0 ])
+  sell_prices.sort() // ascending
+  let sell_price_head = sell_prices[ sell_prices?.length -1 ]
+
+  let midprice = ( buy_price_head + sell_price_head ) / 2
+
+  const sumreduce = (a,b)=>{
+    if ( Number.isFinite(a) && Number.isFinite(b) ){ return (a + b) }
+    else if ( Number.isFinite(b) ){ return (b) }
+    else if ( Number.isFinite(a) ){ return (a) }
+    else { return 0}
+  }
+  let buy_volume =  j_ob?.bids.map(e=>+e[1]).reduce (sumreduce , 0 ) // ( a , b ) => sumreduce(a[ 1 ] , b[ 1 ] ) , 0 )
+  let sell_volume = j_ob?.asks.map(e=>+e[1]).reduce (sumreduce , 0 ) // reduce ( ( a , b ) => sumreduce(a[ 1 ] , b[ 1 ] ) , 0 )
+
+  // let buy_volume =  j_ob?.bids.reduce ( (a,b)=> { +a[ 1 ] + +b[ 1 ] } , 0 )
+  // let sell_volume = j_ob?.asks.reduce ( (a,b)=> +a[ 1 ] + +b[ 1 ] , 0 )
+  return { buy_price_head , sell_price_head , midprice , buy_volume , sell_volume , total_volume: buy_volume + sell_volume }
+}
+/* return val : {
+  buy_price_head: 58532,
+  sell_price_head: 58533,
+  midprice: 58532.5,
+  buy_volume: 6.2794,
+  sell_volume: 2.01002,
+  total_volume: 8.28942
+} */
+let j_ob_ex = {
+  "lastUpdateId": 51764995056,
+  "bids": [
+    [
+      "58532.00000000",
+      "5.59225000"
+    ],
+    [
+      "58531.96000000",
+      "0.00021000"
+    ],
+    [
+      "58531.80000000",
+      "0.34177000"
+    ],
+    [
+      "58531.12000000",
+      "0.00010000"
+    ],
+    [
+      "58531.11000000",
+      "0.00010000"
+    ],
+    [
+      "58531.10000000",
+      "0.06843000"
+    ],
+    [
+      "58531.04000000",
+      "0.00010000"
+    ],
+    [
+      "58530.51000000",
+      "0.11115000"
+    ],
+    [
+      "58530.50000000",
+      "0.13660000"
+    ],
+    [
+      "58530.22000000",
+      "0.02869000"
+    ]
+  ],
+  "asks": [
+    [
+      "58532.01000000",
+      "1.43844000"
+    ],
+    [
+      "58532.02000000",
+      "0.00010000"
+    ],
+    [
+      "58532.03000000",
+      "0.00019000"
+    ],
+    [
+      "58532.04000000",
+      "0.01763000"
+    ],
+    [
+      "58532.49000000",
+      "0.00019000"
+    ],
+    [
+      "58532.50000000",
+      "0.00431000"
+    ],
+    [
+      "58532.96000000",
+      "0.00019000"
+    ],
+    [
+      "58532.97000000",
+      "0.10707000"
+    ],
+    [
+      "58532.98000000",
+      "0.44127000"
+    ],
+    [
+      "58533.00000000",
+      "0.00063000"
+    ]
+  ]
+}
+module.exports= { parse_orderbook , j_ob_ex }
