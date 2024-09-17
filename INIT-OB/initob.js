2578a703f4e543d015923591ded401d5dfcbb7cf
continent2 <appnweb8@gmail.com>
2024-09-17 09:58:26 +0900

    on init-20240917

--- INIT-OB/initob.js
+++ INIT-OB/initob.js
@@ -1,3 +1,20 @@
 
 const db = require( '../models' )
+const axios = require( 'axios')
+let list_tp = [ 'BTC_USDT' , 'ETH_USDT' ]
+const BIN_EP_SPOT_TICKER = `https://api.binance.com/api/v3/ticker/price?symbol=`
+// EX: `https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT`
 
+const main = async ( { } )=>{
+  
+}
+
+main ()
+
+/* 
+QUERY MARKET INFO => TRADE PAIR ,  MIN ORDER AMOUNT , DECIMALS
+FETCH BINANCE TICKER 
+SET BIN SIZE , MIN STEP SIZE ALONG PRICE AXIS
+PLACE LIMIT SELL ORDERS
+PLACE LIMIT BUY ORDERS
+*/
